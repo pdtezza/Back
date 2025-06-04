75c4d6d1e35d95e41972f0764e61868552fd13fb
pdtezza <pdtezza@gmail.com>
2025-06-04 18:30:38 -0300

    commit 1

---
+++ src/main/java/com/example/appReceitas/AppReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.appReceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appReceitas/AppReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appReceitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
